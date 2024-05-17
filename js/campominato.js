79db90b356020ba782c625799063f7ba1568d356
LorezoParis1996 <lorenzoparis86@live.it>
2024-05-17 18:07:20 +0200

    creati elementi in js e personalizzati in css per provare la creazione griglia

--- js/campominato.js
+++ js/campominato.js
@@ -1,2 +1,10 @@
 const gridSection = document.querySelector('section#grid');
 const newLevel = document.querySelector('button#playbtn')
+
+for (let index = 0; index < 100; index++) {
+    const squareArticle = document.createElement('article');
+    squareArticle.classList.add('square');
+
+    gridSection.appendChild(squareArticle);
+
+}
